b8a058fd7982074d860cc85e69843536b861f3bb
stingclaw <thechipthrasher@gmail.com>
2016-02-10 15:49:55 -0500

    Migrate OS to Linux (XUbuntu)

--- README.md
+++
@@ -1,1 +0,0 @@
-# stingclaw.github.io
---
+++ page.js
@@ -0,0 +1,25 @@
+function init() {
+  $('.projects').show();
+  $('.blog').hide();
+  $('.bacon').hide();
+}
+
+$('#item-1').click(function() {
+  $('.projects').show();
+  $('.blog').hide();
+  $('.bacon').hide();
+});
+
+$('#item-2').click(function() {
+  $('.projects').hide();
+  $('.blog').show();
+  $('.bacon').hide();
+});
+
+$('#item-3').click(function() {
+  $('.projects').hide();
+  $('.blog').hide();
+  $('.bacon').show();
+});
+
+$(document).ready(init);
