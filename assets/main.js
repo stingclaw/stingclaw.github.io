008163538c61e957b8d210ce2f908fb14d090d54
Chip Thrasher <thechipthrasher@gmail.com>
2016-11-06 15:23:13 -0500

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -2,23 +2,23 @@ $('#nav a').hover(function() {
   $(this).animate({
     backgroundColor: '#f5f5f5',
     color: '#00a6ff',
-  }, 200);
+  }, 100);
 }, function() {
   $(this).animate({
     backgroundColor: '#ddd',
     color: 'black'
-  }, 200);
+  }, 50);
 });
 $('.project').hover(function() {
   $(this).animate({
     backgroundColor: '#00a6ff',
     color: 'white',
-  }, 200);
+  }, 100);
 }, function() {
   $(this).animate({
     backgroundColor: '#f5f5f5',
-    color: 'darker',
-  }, 200);
+    color: '#0053b3',
+  }, 50);
 });
 function random(array) {
   if(typeof(array) == 'Object') {
